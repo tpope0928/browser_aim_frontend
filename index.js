2d70496d8463ae7a67dadbec20557d522071b113
tpope0928 <tpope0928@gmail.com>
2020-06-06 19:39:33 -0400

    connecting front and back

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
-console.log("in index.js");
+const endPoint = "http://localhost:3000/api/v1/games"
+
+document.addEventListener('DOMContentLoaded', () => {
+    fetch(endPoint)
+        .then(response => response.json())
+        .then(games => {
+            console.log(games);
+        })
+})
