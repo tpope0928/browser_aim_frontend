711131bd0d8bd7d4d56c5703658467e2cc90ec3c
tpope0928 <tpope0928@gmail.com>
2020-06-06 20:24:48 -0400

    end of day save

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const endPoint = "http://localhost:3000/api/v1/games"
 
 document.addEventListener('DOMContentLoaded', () => {
+    getGames()
+})
+
+function getGames() {
     fetch(endPoint)
-        .then(response => response.json())
+        .then(r => r.json())
         .then(games => {
             console.log(games);
         })
-})
+
+}
